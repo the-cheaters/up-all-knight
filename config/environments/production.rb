323e181f2c67cc3186b3f2e141ec7817bd9f407c
Matt Taylor <makproductions06@gmail.com>
2016-11-15 11:11:01 -0500

    changed mailer configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  #config.action_mailer.default_url_options = { host: 'herokuapp'}
+  config.action_mailer.default_url_options = { host: 'up-all-knight.herokuapp.com'}
   # Code is not reloaded between requests.
   config.cache_classes = true
   
