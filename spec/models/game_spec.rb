65d1c68a3527d266a0ed6682583251e851da18b6
Matt Taylor <makproductions06@gmail.com>
2016-11-18 18:09:36 -0500

    removed old tests from game_spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,19 +5,7 @@ RSpec.describe Piece, type: :model do
 end
 
 RSpec.describe Game, type: :model do
-  describe "is_available" do
-    it "should return default value" do
-      game = FactoryGirl.create(:game)
-      expect(game.available).to eq(true)
-    end
-  end
-  describe "is_available" do
-    it "should return false" do
-      game = FactoryGirl.create(:game)
-      game.available = false
-      expect(game.available).to eq(false)
-    end
-  end
+  
   describe "is_available" do
     
     it "should return only games that are available" do
