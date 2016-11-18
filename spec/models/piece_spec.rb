9b9622f41ec2d6d2a2c477a6c87c2d83da441e7c
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-18 22:18:17 +0000

    Checks for horizontal obstruction, all tests passing

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -41,6 +41,34 @@ RSpec.describe Piece, type: :model do
 
     end
 
+    context "right" do
+      let (:destination_x) { 7 }
+      let (:destination_y) { 4 }
+
+      it "should return false if it's not horizontally blocked" do
+        expect(subject).to eq(false)
+      end
+
+      it "should return true if it's horizontally blocked" do
+        FactoryGirl.create(:piece, x_position: 6, y_position: 4, game_id: game.id, player_id: black_player.id)
+        expect(subject).to eq(true)
+      end
+    end
+
+    context "left" do
+      let (:destination_x) { 0 }
+      let (:destination_y) { 4 }
+
+      it "should return false if it's not horizontally blocked" do
+        expect(subject).to eq(false)
+      end
+
+      it "should return true if it's horizontally blocked" do
+        FactoryGirl.create(:piece, x_position: 1, y_position: 4, game_id: game.id, player_id: black_player.id)
+        expect(subject).to eq(true)
+      end
+    end
+
   end
   
 end
