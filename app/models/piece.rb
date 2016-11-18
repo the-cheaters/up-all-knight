926bc2c4b78e724a7058da68f7d81bee0a5c69a8
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-18 22:11:06 +0000

    Checks for vertical obstruction, all tests passing

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -8,7 +8,7 @@ class Piece < ActiveRecord::Base
     location_y = self.y_position
     if location_x == destination_x
       # check for vertical obstruction
-      incrementer = -1 if location_y > destination_y else 1
+      location_y > destination_y ? incrementer = -1 : incrementer = 1
       position_y = location_y + incrementer
       while position_y != destination_y
         if game.pieces.where(x_position: location_x, y_position: position_y).any?
@@ -19,8 +19,18 @@ class Piece < ActiveRecord::Base
       return false
     elsif location_y == destination_y
       # check for horizontal obstruction
+      location_x > destination_x ? incrementer = -1 : incrementer = 1
+      position_x = location_x + incrementer
+      while position_x != destination_x
+        if game.pieces.where(x_position: position_x, y_position: location_y).any?
+          return true
+        end
+        position_x += incrementer
+      end
+      return false
     elsif (location_x - destination_x).abs == (location_y - destination_y).abs
       # check for diagonal obstruction
+      return false
     end
   end
   
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
 
-  let(:game) { FactoryGirl.create(:game) }
-  let(:piece) { FactoryGirl.create(:piece, x_position: 4, y_position: 4, game_id: game.id) }
+  let(:white_player) { FactoryGirl.create(:player, email: 'blah@blah.com', password: 'SPACECAT') }
+  let(:black_player) { FactoryGirl.create(:player, email: 'meow@meow.com', password: 'MONORAILCAT') }
+  let(:game) { FactoryGirl.create(:game, white_player_id: white_player.id, black_player_id: black_player.id) }
+  let(:piece) { FactoryGirl.create(:piece, x_position: 4, y_position: 4, game_id: game.id, player_id: white_player.id) }
 
   describe "Piece#is_obstructed?" do
 
@@ -18,7 +20,22 @@ RSpec.describe Piece, type: :model do
       end
 
       it "should return true if it's vertically blocked" do
-        FactoryGirl.create(:piece, x_position: 4, y_position: 6, game_id: game.id)
+        FactoryGirl.create(:piece, x_position: 4, y_position: 6, game_id: game.id, player_id: black_player.id)
+        expect(subject).to eq(true)
+      end
+
+    end
+
+    context "down" do
+      let(:destination_x) { 4 }
+      let(:destination_y) { 0 }
+
+      it "should return false if it's not vertically blocked" do
+        expect(subject).to eq(false)
+      end
+
+      it "should return true if it's vertically blocked" do
+        FactoryGirl.create(:piece, x_position: 4, y_position: 1, game_id: game.id, player_id: black_player.id)
         expect(subject).to eq(true)
       end
 
