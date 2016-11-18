a3e3bab992f9a95b294818991aa1d3cdb4f7f16e
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-18 22:32:34 +0000

    Tests for up and left passing

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -83,6 +83,20 @@ RSpec.describe Piece, type: :model do
       end
     end
 
+    context "up and left" do
+      let (:destination_x) { 1 }
+      let (:destination_y) { 7 }
+
+      it "should return false if it's not diagonally blocked" do
+        expect(subject).to eq(false)
+      end
+
+      it "should return true if it's diagonally blocked" do
+        FactoryGirl.create(:piece, x_position: 2, y_position: 6, game_id: game.id, player_id: black_player.id)
+        expect(subject).to eq(true)
+      end
+    end
+
   end
   
 end
