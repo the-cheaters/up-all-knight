f1de62a15aab31dec3bb19af0157591c5146d883
Gonzalo <gonsanchezs@gmail.com>
2016-11-19 18:24:42 +0000

    Fix STI, add model and factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,23 +1,27 @@
 FactoryGirl.define do
   factory :player do
-    
   end
+
   factory :piece do
     association :game
     association :player
-    
+
+    factory :pawn do
+      type 'Pawn'
+    end
   end
-  
+
+
   factory :game do
     sequence :id do |n|
       n
     end
   end
-  
+
   # Factories for Omniauth
-  
+
   factory :auth_hash, class: OmniAuth::AuthHash do
-    
+
     initialize_with do
       OmniAuth::AuthHash.new({
         provider: provider,
@@ -27,32 +31,32 @@ FactoryGirl.define do
         }
         })
       end
-      
+
       trait :facebook do
         provider "facebook"
         sequence(:uid)
         email "testuser@facebook.com"
       end
-      
+
       trait :google do
         provider "google"
         sequence(:uid)
         email "testuser@gmail.com"
       end
-      
+
       trait :twitter do
         provider "twitter"
         sequence(:uid)
         email "testuser@twitter.com"
       end
-      
+
       trait :does_not_persist do
         email ""
       end
-      
+
     end
-    
-    
-    
+
+
+
   end
-  
+
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  
+
+  describe 'a Pawn' do
+    it 'should be a pawn' do
+      pawn = create(:pawn)
+      expect(pawn.type).to eq 'Pawn'
+    end
+  end
+
 end
