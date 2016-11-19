56b5ee9ee44006fbec483146cfd905bcd47c2806
Gonzalo <gonsanchezs@gmail.com>
2016-11-19 18:29:30 +0000

    Fix typo in test

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Piece, type: :model do
 
   describe 'a Pawn' do
     it 'should be a pawn' do
-      pawn = create(:pawn)
+      pawn = FactoryGirl.create(:pawn)
       expect(pawn.type).to eq 'Pawn'
     end
   end
