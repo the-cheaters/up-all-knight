3eb8856c1f99f92d0605697d16a62204f4ba295b
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-20 00:49:41 +0000

    Refactor code to use ranges

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -10,22 +10,18 @@ class Piece < ActiveRecord::Base
       # check for vertical obstruction
       location_y > destination_y ? incrementer = -1 : incrementer = 1
       position_y = location_y + incrementer
-      while position_y != destination_y
-        if game.pieces.where(x_position: location_x, y_position: position_y).any?
-          return true
-        end
-        position_y += incrementer
+      range = [position_y, destination_y].min...[position_y, destination_y].max
+      if game.pieces.where(x_position: location_x, y_position: range).any?
+        return true
       end
       return false
     elsif location_y == destination_y
       # check for horizontal obstruction
       location_x > destination_x ? incrementer = -1 : incrementer = 1
       position_x = location_x + incrementer
-      while position_x != destination_x
-        if game.pieces.where(x_position: position_x, y_position: location_y).any?
-          return true
-        end
-        position_x += incrementer
+      range = [position_x, destination_x].min...[position_x, destination_x].max
+      if game.pieces.where(x_position: range, y_position: location_y).any?
+        return true
       end
       return false
     elsif (location_x - destination_x).abs == (location_y - destination_y).abs
@@ -34,12 +30,10 @@ class Piece < ActiveRecord::Base
       location_y > destination_y ? y_incrementer = -1 : y_incrementer = 1
       position_x = location_x + x_incrementer
       position_y = location_y + y_incrementer
-      while position_x != destination_x && position_y != destination_y
-        if game.pieces.where(x_position: position_x, y_position: position_y).any?
-          return true
-        end
-        position_x += x_incrementer
-        position_y += y_incrementer
+      range_x = [position_x, destination_x].min...[position_x, destination_x].max
+      range_y = [position_y, destination_y].min...[position_y, destination_y].max
+      if game.pieces.where(x_position: range_x, y_position: range_y).any?
+        return true
       end
       return false
     end
