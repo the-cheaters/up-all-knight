f24ebb54735cc4269e51ba6ec363c1a5705f4752
Gonzalo <gonsanchezs@gmail.com>
2016-11-20 21:31:25 +0000

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118012333) do
+ActiveRecord::Schema.define(version: 20161119163703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20161118012333) do
     t.integer  "white_player_id"
     t.integer  "current_turn"
     t.integer  "winning_player_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pawns", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
