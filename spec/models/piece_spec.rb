7906c2d704c00676791871f55ffb2d8668ab2ce0
Gonzalo <gonsanchezs@gmail.com>
2016-11-20 21:29:02 +0000

    Move default attributes to factory

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-
   let(:white_player) { FactoryGirl.create(:player, email: 'blah@blah.com', password: 'SPACECAT') }
   let(:black_player) { FactoryGirl.create(:player, email: 'meow@meow.com', password: 'MONORAILCAT') }
   let(:game) { FactoryGirl.create(:game, white_player_id: white_player.id, black_player_id: black_player.id) }
   let(:piece) { FactoryGirl.create(:piece, x_position: 4, y_position: 4, game_id: game.id, player_id: white_player.id) }
+  let(:pawn) { FactoryGirl.create(:pawn, x_position: 2, y_position: 1, game_id: game.id, player_id: black_player.id) }
 
   describe 'a Pawn' do
     it 'should be a pawn' do
-      pawn = FactoryGirl.create(:pawn, x_position: 2, y_position: 1, game_id: game.id, player_id: black_player.id)
+      pawn = FactoryGirl.create(:pawn, player_id: black_player.id)
       expect(pawn.type).to eq 'Pawn'
     end
   end
-
 end
