28da436c390f7a55dc029b8c5fa6ddbc0cd336b0
Pamela Willenz <pam@positek.net>
2016-11-21 02:01:19 +0000

    Adjust test for models#timer_spec, getting rspec errors- won't load, tests won't run

--- spec/models/timer_spec.rb
+++ spec/models/timer_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Timer, type: :model do
-  it "should start timer for current player" do
-    # timer = Time.create(player_id: 300)
-    # expect(time).to be_valid
-  end
 
-  it "should stop timer for current player" do 
-    # expect(time).to eq false > 5.minutes
-  end
+  let(:player) { FactoryGirl.create(:player, current_player.id) }
 
-  it "should reset timer for players" do 
-    
-  end
+    it "should start timer for current player" do
+      FactoryGirl.create(:player, current_player.id)
+      expect(timer).to be_valid
+    end
+
+    it "should stop timer for current player" do 
+      expect(timer).to eq false > 5.minutes
+    end
+
+    it "should reset timer for players" do 
+      expect(timer).to eq 0
+      
+    end
 end
