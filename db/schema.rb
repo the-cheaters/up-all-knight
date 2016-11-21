3840b7ad4c765c190247796388c002c49b5393c7
Pamela Willenz <pam@positek.net>
2016-11-21 02:26:31 +0000

    Attempt at fixing schema more with a rake command to fix rspec, still getting error

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161118012333) do
+ActiveRecord::Schema.define(version: 20161118153508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,4 +65,5 @@ ActiveRecord::Schema.define(version: 20161118012333) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
