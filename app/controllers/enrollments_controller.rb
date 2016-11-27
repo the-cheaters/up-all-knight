89bd8afb139172f6b2b0721700e5d2cfef0c571b
Gonzalo <gonsanchezs@gmail.com>
2016-11-27 15:23:21 +0000

    Add methods to controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,18 @@
 class EnrollmentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    current_available_game.add_player(current_user)
+    current_available_game.save
+  end
+
+  def add_player
+    current_user.enrollments.create(game: current_available_game)
+  end
+
+  private
+
+  def current_available_game
+    @current_available_game ||= Game.is_available.find(params[:id])
+  end
 end
