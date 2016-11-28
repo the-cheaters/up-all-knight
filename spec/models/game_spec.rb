992b3094ea78a4be5dc2ce8255e1cd5d0da7f067
Matt Taylor <makproductions06@gmail.com>
2016-11-28 09:28:01 -0500

    added test for Populate_board

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Piece, type: :model do
       expect(@games.last.name).to eq("touchdown")
     end
   end
+  
+  
+end
+
+RSpec.describe Game, type: :model do
+  
+  describe "populate_board!" do
+    let(:white_player) { FactoryGirl.create(:player, email: 'blah@blah.com', password: 'SPACECAT') }
+    let(:black_player) { FactoryGirl.create(:player, email: 'meow@meow.com', password: 'MONORAILCAT') }
+    let(:game) { FactoryGirl.create(:game, white_player_id: white_player.id, black_player_id: black_player.id) }
+    
+    it "should initialize all pieces on board." do
+      expect(game.pieces.count).to eq(32)
+    end
+    
+  end
+  
+  
 end
