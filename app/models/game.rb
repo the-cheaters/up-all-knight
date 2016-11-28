f8aef657e55ab46663a00e45f546ba36f2253dcb
Matt Taylor <makproductions06@gmail.com>
2016-11-28 16:50:47 -0500

    removed captured: false from piece creation

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,33 +7,33 @@ class Game < ActiveRecord::Base
   def populate_board!
     # black_player_id
     (0..7).each do |i|
-      Pawn.create(player_id: self.black_player_id, game_id: self.id, x_position: i, y_position: 1, captured: false)
+      Pawn.create(player_id: self.black_player_id, game_id: self.id, x_position: i, y_position: 1)
     end
     
-    Rook.create(player_id: self.black_player_id, game_id: self.id, x_position: 0, y_position: 0, captured: false)
-    Rook.create(player_id: self.black_player_id, game_id: self.id, x_position: 7, y_position: 0, captured: false)
-    Knight.create(player_id: self.black_player_id, game_id: self.id, x_position: 1, y_position: 0, captured: false)
-    Knight.create(player_id: self.black_player_id, game_id: self.id, x_position: 6, y_position: 0, captured: false)
-    Bishop.create(player_id: self.black_player_id, game_id: self.id, x_position: 2, y_position: 0, captured: false)
-    Bishop.create(player_id: self.black_player_id, game_id: self.id, x_position: 5, y_position: 0, captured: false)
-    Queen.create(player_id: self.black_player_id, game_id: self.id, x_position: 3, y_position: 0, captured: false)
-    King.create(player_id: self.black_player_id, game_id: self.id, x_position: 4, y_position: 0, captured: false)
+    Rook.create(player_id: self.black_player_id, game_id: self.id, x_position: 0, y_position: 0)
+    Rook.create(player_id: self.black_player_id, game_id: self.id, x_position: 7, y_position: 0)
+    Knight.create(player_id: self.black_player_id, game_id: self.id, x_position: 1, y_position: 0)
+    Knight.create(player_id: self.black_player_id, game_id: self.id, x_position: 6, y_position: 0)
+    Bishop.create(player_id: self.black_player_id, game_id: self.id, x_position: 2, y_position: 0)
+    Bishop.create(player_id: self.black_player_id, game_id: self.id, x_position: 5, y_position: 0)
+    Queen.create(player_id: self.black_player_id, game_id: self.id, x_position: 3, y_position: 0)
+    King.create(player_id: self.black_player_id, game_id: self.id, x_position: 4, y_position: 0)
     
     
     
     # white_player_id
     (0..7).each do |i|
-      Pawn.create(player_id: self.white_player_id, game_id: self.id, x_position: i, y_position: 6, captured: false)
+      Pawn.create(player_id: self.white_player_id, game_id: self.id, x_position: i, y_position: 6)
     end
     
-    Rook.create(player_id: self.white_player_id, game_id: self.id, x_position: 0, y_position: 7, captured: false)
-    Rook.create(player_id: self.white_player_id, game_id: self.id, x_position: 7, y_position: 7, captured: false)
-    Knight.create(player_id: self.white_player_id, game_id: self.id, x_position: 1, y_position: 7, captured: false)
-    Knight.create(player_id: self.white_player_id, game_id: self.id, x_position: 6, y_position: 7, captured: false)
-    Bishop.create(player_id: self.white_player_id, game_id: self.id, x_position: 2, y_position: 7, captured: false)
-    Bishop.create(player_id: self.white_player_id, game_id: self.id, x_position: 5, y_position: 7, captured: false)
-    Queen.create(player_id: self.white_player_id, game_id: self.id, x_position: 3, y_position: 7, captured: false)
-    King.create(player_id: self.white_player_id, game_id: self.id, x_position: 4, y_position: 7, captured: false)
+    Rook.create(player_id: self.white_player_id, game_id: self.id, x_position: 0, y_position: 7)
+    Rook.create(player_id: self.white_player_id, game_id: self.id, x_position: 7, y_position: 7)
+    Knight.create(player_id: self.white_player_id, game_id: self.id, x_position: 1, y_position: 7)
+    Knight.create(player_id: self.white_player_id, game_id: self.id, x_position: 6, y_position: 7)
+    Bishop.create(player_id: self.white_player_id, game_id: self.id, x_position: 2, y_position: 7)
+    Bishop.create(player_id: self.white_player_id, game_id: self.id, x_position: 5, y_position: 7)
+    Queen.create(player_id: self.white_player_id, game_id: self.id, x_position: 3, y_position: 7)
+    King.create(player_id: self.white_player_id, game_id: self.id, x_position: 4, y_position: 7)
   end
   
   
