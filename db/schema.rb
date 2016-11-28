8d445f236bc81cf0f2de5b749217b8ab304f782d
Matt Taylor <makproductions06@gmail.com>
2016-11-28 17:33:01 -0500

    added false as default to captured column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118012333) do
+ActiveRecord::Schema.define(version: 20161128222324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20161118012333) do
     t.integer  "game_id"
     t.integer  "x_position"
     t.integer  "y_position"
-    t.boolean  "captured"
+    t.boolean  "captured",   default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
