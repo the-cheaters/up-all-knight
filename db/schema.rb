b810470528d622170e615be0622aceac6dd9b67a
Joel Choi <joelc316@gmail.com>
2016-11-29 18:29:45 +0000

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115154942) do
+ActiveRecord::Schema.define(version: 20161129180238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161115154942) do
     t.boolean  "captured"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "captured_at"
   end
 
   create_table "players", force: true do |t|
