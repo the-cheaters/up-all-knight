dcb1ea66c6677297918c94f2dbde6f2ce06a2c64
Pamela Willenz <pam@positek.net>
2016-11-29 19:28:38 +0000

    fixed db/schema conflict to fix merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-
-ActiveRecord::Schema.define(version: 20161125162050) do
-
-=======
 ActiveRecord::Schema.define(version: 20161129180238) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +59,6 @@ ActiveRecord::Schema.define(version: 20161129180238) do
   add_index "players", ["email"], name: "index_players_on_email", unique: true, using: :btree
   add_index "players", ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true, using: :btree
 
-
   create_table "timers", force: true do |t|
     t.time     "time"
     t.integer  "player_id"
@@ -76,7 +69,5 @@ ActiveRecord::Schema.define(version: 20161129180238) do
     t.datetime "start_time"
     t.boolean  "running"
   end
-
-
 
 end
