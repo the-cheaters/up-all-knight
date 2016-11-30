8ecfddadbb661f6a0fedb8f5223b4e021755f78b
Pamela Willenz <pam@positek.net>
2016-11-30 16:35:26 +0000

    Add display_piece to chess_board controller, show and piece model - code not working yet

--- app/controllers/chess_boards_controller.rb
+++ app/controllers/chess_boards_controller.rb
@@ -1,7 +1,13 @@
 class ChessBoardsController < ApplicationController
 
   def show
+
+    @chess_board = Piece.piece_display.find_by(:game_id)
+    # @chess_board = Chessboard.find(params[:game_id])
+    # @game = Game.find(params[:id])
+    # @pieces = @game.pieces
     
   end
+  
 
 end
