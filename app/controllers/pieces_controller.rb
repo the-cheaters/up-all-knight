c28a51fca526f316269c252aa998efe361fd85f4
Gonzalo <gonsanchezs@gmail.com>
2016-12-01 22:35:41 +0000

    Fix typo in controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -6,7 +6,7 @@ class PiecesController < ApplicationController
   private
 
   def piece_params
-    params.require(:game).permit(:game_id, :player_id, :piece_id, :new_x_destination, :new_y_destination)
+    params.require(:piece).permit(:game_id, :player_id, :piece_id, :new_x_destination, :new_y_destination)
   end
 
 end
