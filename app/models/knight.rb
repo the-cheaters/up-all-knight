2d6582d036b23d3abb414ba5ecce1ddc7565487f
Matt Taylor <makproductions06@gmail.com>
2016-12-01 19:48:15 -0500

    reworked knight#valid_move logic

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -2,8 +2,13 @@ class Knight < Piece
   def valid_move?(destination_x, destination_y)
     valid = super(destination_x, destination_y)
     if valid
-      if (destination_x - self.x_position).abs != 2 && (destination_y - self.y_position).abs != 1 && (destination_x - self.x_position).abs != 1 && (destination_y - self.y_position).abs != 2
-        valid = false
+      byebug
+      if (destination_x - self.x_position).abs != 2 || (destination_y - self.y_position).abs != 1
+        byebug
+        if (destination_x - self.x_position).abs != 1 || (destination_y - self.y_position).abs != 2
+          byebug
+          valid = false
+        end
       end
     end
     return valid
