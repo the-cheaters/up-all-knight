e95ccff0d7d75f0761cc0a6ce202deda5cb741ef
Pamela Willenz <pam@positek.net>
2016-12-05 23:27:37 +0000

    refactor pawn model, nix promotion method and test, add diagonal test

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -10,37 +10,51 @@ class Pawn < Piece
 
   def valid_move?(destination_x, destination_y)
     valid = super(destination_x, destination_y)
-    if valid
-      if (destination_x - self.x_position).abs > 0 || (destination_y - self.y_position).abs > 1
-        valid = false
+    if valid 
+      if pawn_diagonal?(destination_x, destination_y)
+        capture_piece(destination_x, destination_y)
+        return true
+      elsif move_backward?(destination_y) && (one_square?(destination_x, destination_y) || two_squares?(destination_x, destination_y))
+        return false
       end
+      move_backward?(destination_y) 
     end
     return valid
-
-    # pawns can't move backwards
-    return false if self.get_color == WHITE && down?(destination_y)
-    return false if self.get_color == BLACK && up?(destination_y)
-
-    # conditions for pawn
-    one_square?(destination_x, destination_y) ||
-    two_sqaures?(destination_x, destination_y) ||
-    (pawn_diagonal?(destination_x, destination_y) && capture_piece(x, y))
   end
 
   def on_home_row?
-    return true if (self.get_color == WHITE && y_position == 6) || y_position == 1
+    return true if (self.get_color == WHITE && y_position == 1) || y_position == 6
   end
 
   def one_square?(destination_x, destination_y)
-    return (destination_x – self.x_position).abs == 0 && (destination_y – self.y_position).abs == 1
+    return !on_home_row? && ((destination_x – self.x_position).abs > 0 || (destination_y – self.y_position).abs > 1)
   end
 
   def two_squares?(destination_x, destination_y)
-    return on_home_row? && (destination_x - self.x_position).abs == 0 && (destination_y - self.y_position).abs == 2
+    return on_home_row? && ((destination_x - self.x_position).abs > 0 || (destination_y - self.y_position).abs > 2)
   end
 
   def pawn_diagonal?(destination_x, destination_y)
-    diagonal?(destination_x, destination_y) && (destination_x – self.x_position) == 1
+    if game.is_piece_present?(destination_x, destination_y) 
+      return (destination_y - self.y_position).abs == 1 && (destination_x – self.x_position).abs == 1
+    else
+      return false
+    end
   end
+
+  def move_backward?(destination_y)
+    return self.get_color == WHITE && (destination_y - self.y_position) > 0
+    return self.get_color == BLACK && (destination_y - self.y_position) < 0
+  end
+
+  # def move_horizontal?(destination_x)
+  #   byebug
+  #   (x_position - destination_x).abs != 0
+  # end
+
+  # def promotion?(destination_y)
+  #   byebug
+  #   destination_y == 7 && self.get_color == WHITE || destination_y == 0 && self.get_color == BLACK
+  # end
 
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,45 +1,74 @@
-describe "Pawn#valid_move?(destination_x, destination_y)" do
-    
-    let(:white_player) { FactoryGirl.create(:player, id: 100, email: 'blah@blah.com', password: 'SPACECAT') }
-    let(:pawn) { FactoryGirl.create(:pawn, game_id: game.id, player_id: white_player.id) }
-    let(:game) { FactoryGirl.create(:game) }
-    
-    subject { pawn.valid_move?(destination_x, destination_y) }
-    
-    context "valid move for first move" do
-      let(:destination_x) { 1 }
-      let(:destination_y) { 2 }
-      
-      it "should return true if the white pawn is moving 1 or 2 squares up for first move" do
-        expect(subject).to eq(true)
-      end
-    end
+require 'rails_helper'
 
-     context "valid move for any move" do
-      let(:destination_x) { 1 }
-      let(:destination_y) { 2 } 
+RSpec.describe Pawn, type: :model do
 
-      it "should return true if the white pawn is moving 1 square up for any move" do 
-        expect(subject).to eq(true)
-      end
-    end
-    
-    context "invalid move" do
-      let(:destination_x) { 4 }
-      let(:destination_y) { 3 }
+  describe "Pawn#valid_move?(destination_x, destination_y)" do
       
-      it "should return false if the pawn tries to move outside of its constraints" do
-        expect(subject).to eq(false)
-      end
-    end
-    
-    context "no move" do
-      let(:destination_x) { 4 }
-      let(:destination_y) { 4 }
+      let(:white_player) { FactoryGirl.create(:player, id: 100, email: 'blah@blah.com', password: 'SPACECAT') }
+      let(:black_player) { FactoryGirl.create(:player, id: 101, email: 'blah@cat.com', password: 'ONECAT') }
+      let(:game) { FactoryGirl.create(:game, white_player_id: white_player.id, black_player_id: black_player.id) }
+      let(:pawn) { FactoryGirl.create(:pawn, game_id: game.id, player_id: white_player.id) }
+      let(:king) { FactoryGirl.create(:king, x_position: 2, y_position: 2, game_id: game.id, player_id: black_player.id) }
       
-      it "should return false if the pawn tries to move on top of itself" do
-        expect(subject).to eq(false)
+      
+      subject { pawn.valid_move?(destination_x, destination_y) }
+      
+      context "valid move for first move" do
+        let(:destination_x) { 1 }
+        let(:destination_y) { 3 }
+        
+        it "should return true if the white pawn is moving 2 squares up for first move" do
+          expect(subject).to eq(true)
+        end
       end
+
+       context "valid move for any move" do
+        let(:destination_x) { 1 }
+        let(:destination_y) { 2 } 
+
+        it "should return true if the white pawn is moving 1 square up for any move" do 
+          expect(subject).to eq(true)
+        end
+      end
+
+        context "diagonal move for capture" do 
+          let(:destination_x) { 2 }
+          let(:destination_y) { 2 }
+
+          it "should return false if no piece present" do 
+            expect(subject).to eq(false)
+          end
+        end
+
+        context "diagonal move for capture" do 
+          let(:destination_x) { 2 }
+          let(:destination_y) { 2 }
+
+          it "should return true if a piece is present" do 
+            FactoryGirl.create(:pawn, x_position: 2, y_position: 2, player_id: black_player.id)
+            # Piece.create(type: pawn, x_position: 2, y_position: 2, player_id: black_player.id)
+            expect(subject).to eq(true)
+          end
+        end
+
+      #   context "valid move for promotion" do
+      #   let(:destination_x) { 6 }
+      #   let(:destination_y) { 7 } 
+
+      #   it "should return true if the white pawn moves to last row" do 
+      #     expect(subject).to eq(true)
+      #   end
+      # end
+      
+        context "invalid move" do
+          let(:destination_x) { 4 }
+          let(:destination_y) { 3 }
+          
+          it "should return false if the pawn tries to move outside of its constraints" do
+            expect(subject).to eq(false)
+          end
+        end
+     
+      
     end
-    
   end
