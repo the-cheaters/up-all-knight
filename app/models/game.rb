6d16ee4475bc7a3dc672ba8c57d0b08402bad639
Matt Taylor <makproductions06@gmail.com>
2016-12-09 15:27:08 -0500

    added right varibles for partial

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,15 @@
 class Game < ActiveRecord::Base
-
+  
   scope :is_available, -> { where("black_player_id is null or white_player_id = 0") }
-
+  has_many :timers
   after_create :populate_board!
-
+  
   def populate_board!
     # black_player_id
     (0..7).each do |i|
       Pawn.create(player_id: self.black_player_id, game_id: self.id, x_position: i, y_position: 1)
     end
-
+    
     Rook.create(player_id: self.black_player_id, game_id: self.id, x_position: 0, y_position: 0)
     Rook.create(player_id: self.black_player_id, game_id: self.id, x_position: 7, y_position: 0)
     Knight.create(player_id: self.black_player_id, game_id: self.id, x_position: 1, y_position: 0)
@@ -18,14 +18,14 @@ class Game < ActiveRecord::Base
     Bishop.create(player_id: self.black_player_id, game_id: self.id, x_position: 5, y_position: 0)
     Queen.create(player_id: self.black_player_id, game_id: self.id, x_position: 3, y_position: 0)
     King.create(player_id: self.black_player_id, game_id: self.id, x_position: 4, y_position: 0)
-
-
-
+    
+    
+    
     # white_player_id
     (0..7).each do |i|
       Pawn.create(player_id: self.white_player_id, game_id: self.id, x_position: i, y_position: 6)
     end
-
+    
     Rook.create(player_id: self.white_player_id, game_id: self.id, x_position: 0, y_position: 7)
     Rook.create(player_id: self.white_player_id, game_id: self.id, x_position: 7, y_position: 7)
     Knight.create(player_id: self.white_player_id, game_id: self.id, x_position: 1, y_position: 7)
@@ -35,18 +35,18 @@ class Game < ActiveRecord::Base
     Queen.create(player_id: self.white_player_id, game_id: self.id, x_position: 3, y_position: 7)
     King.create(player_id: self.white_player_id, game_id: self.id, x_position: 4, y_position: 7)
   end
-
-
-
+  
+  
+  
   has_many :pieces
   has_many :players
-
+  
   def is_piece_present?(x, y)
     self.pieces.where(x_position: x, y_position: y).any?
   end
-
+  
   def get_piece(x, y)
     self.pieces.where(x_position: x, y_position: y).take
   end
-
+  
 end
