ace41488c9cc67fe621fdaf3199a9402d3217644
Kelly Lougheed <kellylougheed@gmail.com>
2016-12-11 18:24:08 +0000

    Remove stray files

--- db/migrate/20161209201132_add_draw_to_games.rb
+++
@@ -1,8 +0,0 @@
-class AddDrawToGames < ActiveRecord::Migration
-  def change
-    add_column :games, :white_draw, :boolean
-    add_column :games, :black_draw, :boolean
-    add_column :games, :white_forfeit, :boolean
-    add_column :games, :black_forfeit, :boolean
-  end
-end
