82c0417ed5eb61d8fa1861a0bd970183b8f830fe
Joel Choi <joelc316@gmail.com>
2016-12-12 23:44:08 +0000

    Add valid_move? method to bishop model and create bishop_spec.rb file and add proper tests

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,5 +1,11 @@
 class Bishop < Piece
 
+  def valid_move?(destination_x, destination_y)
+    valid = super(destination_x, destination_y)
+    valid && diagonal(destination_x, destination_y)
+
+  end
+
   def unicode_symbol
     if self.get_color == WHITE
       return "&#9815;"
