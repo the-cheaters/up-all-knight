4fe1ce35b08a90fa949f65f0c06985be4a45368b
Matt Taylor <makproductions06@gmail.com>
2016-12-13 09:33:56 -0500

    remove hardcoded ids

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
-  let(:white_player) { FactoryGirl.create(:player, id: 100, email: 'blah@blah.com', password: 'SPACECAT') }
-  let(:black_player) { FactoryGirl.create(:player, id: 101, email: 'meow@meow.com', password: 'MONORAILCAT') }
+  let(:white_player) { FactoryGirl.create(:player, email: 'blah@blah.com', password: 'SPACECAT') }
+  let(:black_player) { FactoryGirl.create(:player, email: 'meow@meow.com', password: 'MONORAILCAT') }
   let(:game) { FactoryGirl.create(:game,:populated, white_player_id: white_player.id, black_player_id: black_player.id) }
   let(:knight) { FactoryGirl.create(:knight, game_id: game.id,x_position: 4, y_position: 4 ) }
   
-  before(:all) do
-    Rails.cache.clear
-  end
   
   it "has a 200 status code" do
     patch :update, game_id: game.id, id: knight.id , piece: { x_position: '2', y_position: '3' }
