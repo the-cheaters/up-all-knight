69e85658006af07b9c9d3519e69ac4353b190508
Pamela Willenz <pam@positek.net>
2016-12-14 21:20:15 +0000

    add color check, tests pass

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,4 +1,12 @@
 class King < Piece
+
+  def y_position
+    y_position = 0
+    if self.get_color == WHITE
+      y_position = 7
+    end
+    y_position
+  end  
   
   def valid_move?(destination_x, destination_y)
     valid = super(destination_x, destination_y)
@@ -12,6 +20,8 @@ class King < Piece
     return valid
   end
 
+  
+
 # CASTLING KING
 
   def move_to(destination_x, destination_y)
@@ -19,6 +29,7 @@ class King < Piece
     if valid
       castle!(destination_x, destination_y) 
     end
+    valid
   end
 
   def can_castle?(destination_x, destination_y)
@@ -32,17 +43,17 @@ class King < Piece
 
   def castle!(destination_x, destination_y)
     if castling_kingside?(destination_x, destination_y)
-      rook = game.pieces.where(x_position: 7, y_position: 0, player_id: player_id, type: "Rook").last
+      rook = game.pieces.where(x_position: 7, y_position: y_position, player_id: player_id, type: "Rook").last
       rook.update_attributes(x_position: 5)
     end
     if castling_queenside?(destination_x, destination_y)
-      rook = game.pieces.where(x_position: 0, y_position: 0, player_id: player_id, type: "Rook").last 
+      rook = game.pieces.where(x_position: 0, y_position: y_position, player_id: player_id, type: "Rook").last 
       rook.update_attributes(x_position: 3)
     end
   end
 
   def castling_kingside?(destination_x, destination_y)
-    if (destination_x == 6 && destination_y == 0) && 
+    if (destination_x == 6 && destination_y == y_position) && 
         (is_rook_kingside? == true)
       return true
     else
@@ -51,7 +62,7 @@ class King < Piece
   end
 
   def castling_queenside?(destination_x, destination_y)
-    if (destination_x == 2 && destination_y == 0) && 
+    if (destination_x == 2 && destination_y == y_position) && 
         (is_rook_queenside? == true)
       return true
     else 
@@ -60,7 +71,7 @@ class King < Piece
   end
 
   def is_rook_queenside?
-    rook = game.pieces.where(x_position: 0, y_position: 0, player_id: player_id, type: "Rook").last
+    rook = game.pieces.where(x_position: 0, y_position: y_position, player_id: player_id, type: "Rook").last
     if rook != nil
       if rook.moves == 0 
         return true
@@ -70,7 +81,7 @@ class King < Piece
   end
 
   def is_rook_kingside?
-    rook = game.pieces.where(x_position: 7, y_position: 0, player_id: player_id, type: "Rook").last 
+    rook = game.pieces.where(x_position: 7, y_position: y_position, player_id: player_id, type: "Rook").last 
     if rook != nil
       if rook.moves == 0 
         return true
