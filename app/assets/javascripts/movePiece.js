71c137cf781f2ed75e2877fca8bcdf6d8c078456
Matt Taylor <makproductions06@gmail.com>
2016-12-26 21:46:50 -0500

    fixed undefined players turn

--- app/assets/javascripts/movePiece.js
+++ app/assets/javascripts/movePiece.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
 
-  window.broadcast_channel.bind('move_piece_onscreen', function(data) {
-    currentLocation = "#C" + data.x_position + "R" + data.y_position
-    destination = "#C" + data.destination_x + "R" + data.destination_y
-    $(currentLocation).children(".piece").html("")
-    $(destination).children(".piece").html(data.symbol)
-  })
-
   var currentPlayer = $('.game-wrapper').data('player')
   var currentPlayersTurn = $('.game-wrapper').data('current-turn')
   var currentTimer = null
