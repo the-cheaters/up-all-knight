bac1848deafb0304bb9abf369d9f51983600b07f
Matt Taylor <makproductions06@gmail.com>
2016-12-26 21:24:32 -0500

    removed some rogue code

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,10 +2,6 @@ class PiecesController < ApplicationController
   before_action :set_game
   def update
     if selected_piece.move_to(params[:piece][:x_position].to_i,params[:piece][:y_position].to_i)
-      Pusher['broadcast'].trigger!('move_piece_onscreen', {
-        symbol: selected_piece.unicode_symbol, destination_x: params[:piece][:x_position].to_i, destination_y: params[:piece][:y_position].to_i, x_position: selected_piece.x_position, y_position: selected_piece.x_position
-        })
-      
         if selected_piece.can_pawn_promote?(params[:piece][:y_position].to_i)
            Pusher["private-user_#{current_player.id}"].trigger!('pawn-promotion', {
           pawnPromotionUpdateURL: game_piece_promote_pawn_path(@game, selected_piece.id), message: "You made it to the other side! Click one of the buttons to pick your new piece."
@@ -17,7 +13,6 @@ class PiecesController < ApplicationController
           Pusher['broadcast'].trigger!('start_game', {
             has_started: true })
         end
-        Pusher["broadcast"].trigger!('change_turns',{ current_turn: selected_piece.game.current_player_turn })
       if selected_piece.game.is_blitz
         @white_timer = selected_piece.game.timers.where(player_id: selected_piece.game.white_player_id).last
         @black_timer = selected_piece.game.timers.where(player_id: selected_piece.game.black_player_id).last
@@ -31,6 +26,7 @@ class PiecesController < ApplicationController
         Pusher["broadcast"].trigger!('change_turns',{timer: {white_start_time: @white_timer.start_time, black_start_time: @black_timer.start_time, black_time_left: @black_timer.time_left,white_time_left: @white_timer.time_left}, current_turn: selected_piece.game.current_player_turn })
         render json: {}, status: :ok
       else
+        Pusher["broadcast"].trigger!('change_turns',{ current_turn: selected_piece.game.current_player_turn })
         render json: {}, status: :ok
       end
     else
